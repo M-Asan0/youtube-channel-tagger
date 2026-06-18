efd5627f7ef6e1e3ebd64d58ded439bbf3f60f7a
asano <asanomasaaki6@gmail.com>
2026-06-18 16:20:34 +0900

    refactor: extract TagEditModal component from TagManagement

--- src/components/TagManagement.tsx
+++ src/components/TagManagement.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { saveAppData } from "../storage";
+import { TagEditModal } from "./TagEditModal"
 import type { AppData, Tag } from "../types";
 
 type TagManagementProps = {
@@ -197,80 +198,7 @@ export function TagManagement({
         </table>
       )}
 
-      {editingTag && (
-        <div
-          style={{
-            position: "fixed",
-            inset: 0,
-            background: "rgba(0, 0, 0, 0.4)",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            zIndex: 100,
-          }}
-        >
-          <div
-            style={{
-              background: "#fff",
-              padding: 16,
-              borderRadius: 8,
-              minWidth: 320,
-            }}
-          >
-            <h3>Edit Tag</h3>
-
-            <div>
-              <label>Tag Name</label>
-              <br />
-              <input
-                value={editingTag.name}
-                onChange={(e) =>
-                  setEditingTag({
-                    ...editingTag,
-                    name: e.target.value,
-                  })
-                }
-              />
-            </div>
-
-            <div style={{ marginTop: 8 }}>
-              <label>Color</label>
-              <br />
-              <input
-                value={editingTag.color}
-                onChange={(e) =>
-                  setEditingTag({
-                    ...editingTag,
-                    color: e.target.value,
-                  })
-                }
-              />
-
-              <span
-                style={{
-                  display: "inline-block",
-                  width: 20,
-                  height: 20,
-                  background: editingTag.color,
-                  border: "1px solid #ccc",
-                  marginLeft: 8,
-                  verticalAlign: "middle",
-                }}
-              />
-            </div>
-
-            <div style={{ marginTop: 16 }}>
-              <button onClick={updateTag}>Save</button>
-              <button
-                onClick={() => setEditingTag(null)}
-                style={{ marginLeft: 8 }}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
+      {editingTag && (<TagEditModal editingTag={editingTag} setEditingTag={setEditingTag} updateTag={updateTag} />)}
     </section>
   );
 }
